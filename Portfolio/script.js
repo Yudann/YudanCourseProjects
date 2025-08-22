d7c4b823a511dd47c8c651006c4e1c00deb97c89
Yudann <yudannnnn80@gmail.com>
2025-08-22 15:13:53 +0700

    refactor: update folder name

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const mobileNav = document.querySelector(".mobile-nav");
-
-hamburger.addEventListener("click", () => {
-  mobileNav.classList.toggle("active");
-  hamburger.classList.toggle("open");
-});
